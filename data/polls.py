aed788200484a8b1d18f6579de1c9ccb0da1fa17
Egor <edkuz11@yandex.ru>
2021-04-07 18:39:02 +0300

    Option model

--- data/polls.py
+++ data/polls.py
@@ -9,7 +9,7 @@ from data.db_session import SqlAlchemyBase
 class Poll(SqlAlchemyBase):
     __tablename__ = "polls"
 
-    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True, autoincrement=True)
     title = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=False)
     description = sqlalchemy.Column(sqlalchemy.String, nullable=False)
     author = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
@@ -20,3 +20,16 @@ class Poll(SqlAlchemyBase):
 
     def __repr__(self):
         return f"<Poll> {self.id} {self.title}"
+
+
+class Option(SqlAlchemyBase):
+    __tablename__ = "options"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, index=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    poll = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey(Poll.id))
+
+    poll_obj = orm.relation(Poll)
+
+    def __repr__(self):
+        return f"<Option> {self.id} {self.title}"

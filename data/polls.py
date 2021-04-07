f324a4637c85a362d5c61bc30b2d5d820f218fcd
Egor <edkuz11@yandex.ru>
2021-04-07 16:27:54 +0300

    Poll model

---
+++ data/polls.py
@@ -0,0 +1,19 @@
+import sqlalchemy
+from sqlalchemy import orm
+
+from data.db_session import SqlAlchemyBase
+
+
+class Poll(SqlAlchemyBase):
+    __tablename__ = "polls"
+
+    id = sqlalchemy.Column(sqlalchemy.Integer, primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, index=True, nullable=False)
+    description = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    author = sqlalchemy.Column(sqlalchemy.Integer, sqlalchemy.ForeignKey("users.id"))
+    completed = sqlalchemy.Column(sqlalchemy.Boolean, default=False)
+
+    author_obj = orm.relation("User")
+
+    def __repr__(self):
+        return f"<Poll> {self.id} {self.title}"

e9128559fb82a7a336777cc5b8ff04e401062689
Egor <edkuz11@yandex.ru>
2021-04-11 14:34:21 +0300

    API requests headers

--- tools/api_requests.py
+++ tools/api_requests.py
@@ -1,27 +1,31 @@
 from flask import request
-from requests import get, post, put, delete
+import requests
 
 
 class ApiRequest:
     request_function = None
 
     @classmethod
-    def make_request(cls, url_parts, **kwargs):
+    def make_request(cls, *url_parts, **kwargs):
         url = request.url_root + "api/" + "/".join(map(str, url_parts))
-        return cls.request_function(url, **kwargs)
+        headers = dict()
+        access_token = request.cookies.get("access_token_cookie")
+        if access_token:
+            headers = {"Authorization": f"Bearer {access_token}"}
+        return cls.request_function(url, headers=headers, **kwargs)
 
 
 class ApiGet(ApiRequest):
-    request_function = get
+    request_function = requests.get
 
 
 class ApiPost(ApiRequest):
-    request_function = post
+    request_function = requests.post
 
 
 class ApiPut(ApiRequest):
-    request_function = put
+    request_function = requests.put
 
 
 class ApiDelete(ApiRequest):
-    request_function = delete
+    request_function = requests.delete

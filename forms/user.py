be3324926b8e8ca97aa03ac128331ce4e88ea541
Egor <edkuz11@yandex.ru>
2021-04-12 10:27:44 +0300

    Adding register form validators

--- forms/user.py
+++ forms/user.py
@@ -1,14 +1,14 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField
 from wtforms.fields.html5 import EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email, EqualTo, Length
 
 
 class RegisterForm(FlaskForm):
-    email = EmailField("Email", validators=[DataRequired()])
-    username = StringField("Username", validators=[DataRequired()])
+    email = EmailField("Email", validators=[DataRequired(), Email()])
+    username = StringField("Username", validators=[DataRequired(), Length(min=4, max=20)])
     password = PasswordField("Password", validators=[DataRequired()])
-    password_again = PasswordField("Repeat password", validators=[DataRequired()])
+    password_again = PasswordField("Repeat password", validators=[DataRequired(), EqualTo("password")])
     submit = SubmitField("Register")
 
 

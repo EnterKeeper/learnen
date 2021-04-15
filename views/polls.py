f288ecd9fe7768f104b9cf124a97a685f005e91f
Egor <edkuz11@yandex.ru>
2021-04-15 16:01:40 +0300

    Polls page small update

--- views/polls.py
+++ views/polls.py
@@ -13,8 +13,8 @@ blueprint = Blueprint(
 @blueprint.route("/polls")
 @jwt_required(optional=True)
 def polls_list():
-    resp = ApiGet.make_request("polls").json()
-    return render_template("polls.html", polls=resp.get("polls"))
+    polls = ApiGet.make_request("polls").json().get("polls")
+    return render_template("polls.html", polls=polls)
 
 
 @blueprint.route("/polls/<int:poll_id>")

577f62bbd8a127428c09ef77de5eb9847a78babc
Egor <edkuz11@yandex.ru>
2021-04-22 00:01:52 +0300

    Config editing

--- qp/config.py
+++ qp/config.py
@@ -1,8 +1,8 @@
 class Config:
-    SECRET_KEY = "Long secret string"
-    JWT_SECRET_KEY = "Long secret string"
+    SECRET_KEY = "VeryLongSecretString"
+    JWT_SECRET_KEY = "VeryLongSecretString"
     PROPAGATE_EXCEPTIONS = True
     JWT_TOKEN_LOCATION = ["cookies", "headers"]
-    JWT_COOKIE_CSRF_PROTECT = False
-    JWT_CSRF_CHECK_FORM = False
-    JWT_ACCESS_TOKEN_EXPIRES = False
+    JWT_COOKIE_SECURE = True
+    JWT_COOKIE_CSRF_PROTECT = True
+
